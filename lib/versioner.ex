3a8289541bbef93d8ff2e18b16ec663547602e2c
Eugene Babushkin <oigen90@gmail.com>
2017-03-27 16:28:22 +0300

    initial commit: empty project

---
+++ lib/versioner.ex
@@ -0,0 +1,18 @@
+defmodule Versioner do
+  @moduledoc """
+  Documentation for Versioner.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Versioner.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/versioner_test.exs
@@ -0,0 +1,8 @@
+defmodule VersionerTest do
+  use ExUnit.Case
+  doctest Versioner
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
